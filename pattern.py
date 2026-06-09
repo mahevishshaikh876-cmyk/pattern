57ae77e3d026dfff8c41735b0d83fe26be3432ac
mahi_shaikh. <mahevishshaikh876@gmail.com>
2026-06-09 14:41:57 +0530

    pattern.py

--- pattern.py
+++ pattern.py
@@ -1,43 +1,66 @@
-n = int(input("Enter rows: "))
-open("pattern.txt", "w").write("")
+n = int(input("Enter rows for Pattern 1: "))
 
-for i in range(n):
-    txt = "####"
-    print(txt)
-    
+with open("pattern.txt", "w") as f:
 
+    # Pattern 1
+    f.write("Pattern 1\n")
+    for i in range(n):
+        txt = "####"
+        print(txt)
+        f.write(txt + "\n")
 
+    f.write("\n")
 
-for i in range(1, 5):
-    txt = ""
-    for j in range(1, i + 1):
-        print(j, end="")
-        txt += str(j)
+    # Pattern 2
+    f.write("Pattern 2\n")
+    for i in range(1, 5):
+        txt = ""
+        for j in range(1, i + 1):
+            txt += str(j)
 
-    print()
-    
+        print(txt)
+        f.write(txt + "\n")
 
-n = int(input("Enter rows: "))
+    f.write("\n")
 
+    # Pattern 3
+    f.write("Pattern 3\n")
+    n2 = int(input("Enter rows for Pattern 3: "))
 
-for i in range(n, 0, -1):
-    print("*" * i)
-    txt = "*" * i
-    
+    for i in range(n2, 0, -1):
+        txt = "*" * i
+        print(txt)
+        f.write(txt + "\n")
 
+    f.write("\n")
 
-for i in range(1, 5):
-    txt = ""
-    for j in range(i):
-        print(i, end="")
-        txt += str(i)
+    # Pattern 4
+    f.write("Pattern 4\n")
+    for i in range(1, 5):
+        txt = str(i) * i
+        print(txt)
+        f.write(txt + "\n")
 
-    print()
-    
+    f.write("\n")
 
+    # Pattern 5
+    f.write("Pattern 5\n")
+    for i in range(1, 5):
+        txt = "*" * i
+        print(txt)
+        f.write(txt + "\n")
 
+    # Pattern 6
+    f.write("Pattern 6\n")
+    num = 1
 
-for i in range(1, 5):
-    txt = "*" * i
-    print(txt)
-    open("pattern.txt", "a").write(txt + "\n")
+    for i in range(1, 5):
+        txt = ""
+        for j in range(i):
+            txt += str(num)
+            num += 1
+
+        print(txt)
+        f.write(txt + "\n")
+
+print("All patterns saved in pattern.txt")
